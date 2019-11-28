4f5bede83c2dda41582e846b90c2e2d46bfade21
Razique Mahroua <rmahroua@redhat.com>
2019-11-28 14:06:02 -0800

    Add SSL support and documentation on how to use the container

---
+++ todo-angular/README.md
@@ -0,0 +1,44 @@
+## About this container
+This container deploys a stand-alone To Do application written in AngularJS.
+The web server supports both HTTP and HTTPs connection.
+
+## How to build
+Run the following command to build the container image:
+`$ podman build -t do280/todo-angular:latest .`
+
+The current application provides a self-signed certificate in `ssl`. Their names
+would match the certificates name in OpenShift.
+If you need to regenerate a self-signed certificate,run the following command:
+`$ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt`
+
+If you need to recreate a Diffie-Hellman group, run the following command:
+`$ openssl dhparam -out dhparam.pem 2048`
+
+## How to run
+
+### In HTTP mode
+`podman run --userns keep-id  -v ./ssl/certs:/usr/local/etc/ssl/certs:Z --name todo -p 8080:8080 do280/todo-angular:latest`
+
+### In HTTPs mode
+`podman run --userns keep-id  -v ./ssl/certs:/usr/local/etc/ssl/certs:Z --name todo -p 8443:8443 do280/todo-angular:latest`
+
+### Disable HTTPs support
+If you need to disable HTTPs support, run the following steps:
+
+  1. In `Dockerfile` -- comment lines 16 and 17:
+  ```
+  # COPY nginx/dhparam.pem /etc/ssl/conf/dhparam.pem
+  # COPY nginx/conf.d/ssl.conf /etc/nginx/conf.d/ssl.conf
+  ```
+  2. In `nginx/nginx.conf`comment line 36
+  ```
+  # include /etc/nginx/conf.d/*.conf; 
+  ```
+  3. Rebuild the image:
+  ```
+  `$ podman build -t do280/todo-angular:latest .`
+  ```
+  4. Run the following command to create the container:
+  ```
+  `$ podman run --name todo -p 8080:8080 do280/todo-angular:latest`
+  ```
--- todo-angular/src/assets/js/app/domain/todoitems.js
+++ todo-angular/src/assets/js/app/domain/todoitems.js
@@ -19,41 +19,25 @@ angular.module('todoItemsApp')
 
         var todoItems = [
             {
-                description: 'Buy bread',
-                category: 'Domestic',
-                complete: false,
-                dueBy: randomInNextFortnight(),
-                cost: 1.75,
-                notes: "Buy from Budgens\nor failing that from Tesco's"
-            },
-            {
-                description: 'Buy milk',
-                category: 'Domestic',
-                complete: false,
-                dueBy: randomInNextFortnight(),
-                cost: 0.75,
-                notes: null
-            },
-            {
-                description: 'Buy stamps',
-                category: 'Domestic',
-                complete: true,
-                dueBy: randomInNextFortnight(),
-                cost: 10.00,
-                notes: null
-            },
-            {
-                description: 'Mow lawn',
+                description: 'Take OpenShift training',
                 category: 'Domestic',
                 complete: false,
                 dueBy: randomInNextFortnight(),
                 cost: null,
+                notes: "Take OpenShift DO280 course"
+            },
+            {
+                description: 'Book exam',
+                category: 'Domestic',
+                complete: false,
+                dueBy: randomInNextFortnight(),
+                cost: 200,
                 notes: null
             },
             {
-                description: 'Organize brown bag',
-                category: 'Professional',
-                complete: false,
+                description: 'Read James Joyce',
+                category: 'Domestic',
+                complete: true,
                 dueBy: randomInNextFortnight(),
                 cost: null,
                 notes: null
@@ -64,30 +48,6 @@ angular.module('todoItemsApp')
                 complete: false,
                 dueBy: randomInNextFortnight(),
                 cost: 7.50,
-                notes: null
-            },
-            {
-                description: 'Sharpen knives',
-                category: 'Domestic',
-                complete: false,
-                dueBy: randomInNextFortnight(),
-                cost: null,
-                notes: null
-            },
-            {
-                description: 'Stage Isis release',
-                category: 'Professional',
-                complete: false,
-                dueBy: randomInNextFortnight(),
-                cost: null,
-                notes: null
-            },
-            {
-                description: 'Submit conference session',
-                category: 'Professional',
-                complete: false,
-                dueBy: randomInNextFortnight(),
-                cost: null,
                 notes: null
             },
             {
@@ -102,14 +62,6 @@ angular.module('todoItemsApp')
                 description: 'Write blog post',
                 category: 'Professional',
                 complete: true,
-                dueBy: randomInNextFortnight(),
-                cost: null,
-                notes: null
-            },
-            {
-                description: 'Write to penpal',
-                category: 'Other',
-                complete: false,
                 dueBy: randomInNextFortnight(),
                 cost: null,
                 notes: null
