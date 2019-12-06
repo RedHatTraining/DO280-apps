10168193764d8ab1813756aba25c25294583bbb8
joelbirchler <jbirchler@redhat.com>
2019-12-05 17:16:11 -0800

    ch11 book app outputs a list of books

--- books/main.go
+++ books/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -11,7 +12,34 @@ import (
 
 const port = "8080"
 
+type book struct {
+	ID            int
+	Title, Author string
+}
+
+var books = []book{
+	{
+		ID:     1,
+		Title:  "The Hitchhiker's Guide to the Galaxy",
+		Author: "Adams, Douglas",
+	},
+	{
+		ID:     2,
+		Title:  "Dirk Gently's Holistic Detective Agency",
+		Author: "Adams, Douglas",
+	},
+	{
+		ID:     3,
+		Title:  "Snow Crash",
+		Author: "Stephenson, Neal",
+	},
+}
+
+var homeTemplate *template.Template
+
 func main() {
+	homeTemplate = template.Must(template.ParseFiles("template.html"))
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/healthz", healthzHandler)
@@ -29,7 +57,7 @@ func main() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "hello")
+	homeTemplate.Execute(w, books)
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
