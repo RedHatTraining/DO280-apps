4e13b2d73eabb5f4c035524a665a1260b24190da
Federico Fapitalle <ffapital@redhat.com>
2021-07-15 16:48:44 -0300

    change database connection to use TLS

--- exoplanets/database.go
+++ exoplanets/database.go
@@ -14,7 +14,7 @@ type dbInfo struct {
 
 func (d dbInfo) String() string {
 	return fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require sslcert=/certs/tls.crt sslkey=/certs/tls.key",
 		d.host, d.port, d.user, d.password, d.dbname)
 }
 
